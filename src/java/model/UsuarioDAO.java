e1d2938ca2b7c9b162a81595d2c910d829495231
Dayvidson <dayvidhms@gmail.com>
2019-05-03 21:56:48 -0300

    Att1

--- src/java/model/UsuarioDAO.java
+++ src/java/model/UsuarioDAO.java
@@ -4,6 +4,7 @@ import conexao.Conexao;
 import controller.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,13 +31,42 @@ public class UsuarioDAO {
             PreparedStatement insere = this.conexao.prepareStatement(insertCliente);
             insere.setString(1, usuario.getNomeCompleto());
             insere.setString(2, usuario.getPhoneWhatsApp());
-            insere.setString(3, usuario.getSenha());
-            insere.setString(4, usuario.getUsuario());
+            insere.setString(3, usuario.getUsuario());
+            insere.setString(4, usuario.getSenha());
 
             insere.execute();
 
         } catch (SQLException ex) {
             ex.printStackTrace();
+        }
+    }
+    
+    public Usuario loginUser(String user, String senha){
+        String select = "select * from usuario where usuario = ? and senha = ?";
+        Usuario usuario;
+        
+        try {
+            PreparedStatement selecionar = this.conexao.prepareStatement(select);
+            
+            selecionar.setString(1, user);
+            selecionar.setString(2, senha);
+            
+            ResultSet dados = selecionar.executeQuery();
+            if(dados.next()){
+                usuario = new Usuario();
+                usuario.setId(dados.getInt("usuario_id"));
+                usuario.setNomeCompleto(dados.getString("nome"));
+                usuario.setPhoneWhatsApp(dados.getString("phone"));
+                usuario.setSenha(dados.getString("phone"));
+                usuario.setUsuario(dados.getString("usuario"));
+                return usuario;
+            }else{
+                return null;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
     }
 }
--- src/java/ws/UsuarioResource.java
+++ src/java/ws/UsuarioResource.java
@@ -11,6 +11,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import model.UsuarioDAO;
 
@@ -45,6 +46,17 @@ public class UsuarioResource {
 
         UsuarioDAO dao = new UsuarioDAO();
         dao.insertUser(usuarioResultado);
+    }
+    
+    @GET
+    @Produces("application/json")
+    @Path("/login")
+    public String login(@QueryParam("user")String user, @QueryParam("senha")String senha){
+        UsuarioDAO dao = new UsuarioDAO();
+        Gson gson = new Gson();
+        return gson.toJson(dao.loginUser(user, senha));
+               
+          
     }
 
 }
